3c2597588be0fb70f9058414a7025078c1a94119
Nickolay Ilyushin <nickolay02@inbox.ru>
2019-03-19 18:50:36 +0200

    Initial commit

---
+++ src/main/java/com/github/handicraftsman/legacymckotlin/LegacyMCKotlin.java
@@ -0,0 +1,9 @@
+package com.github.handicraftsman.legacymckotlin;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = LegacyMCKotlin.MODID, version = LegacyMCKotlin.VERSION)
+public class LegacyMCKotlin {
+    public static final String MODID = "legacymckotlin";
+    public static final String VERSION = "1.0";
+}
